0c5aaeecc35e990723bdd88a8c886adc4a97880c
2529742 <hello.andrey@gmail.com>
2011-11-02 12:36:26 +0100

    fixed bug with image search, created new render function for images

--- vie.widget.terkait.js
+++ vie.widget.terkait.js
@@ -20,7 +20,7 @@ $(window).load(function () {
             leftPos: '0px',                          //position from left/ use if tabLocation is bottom or top
             fixedPosition: false                      //options: true makes it stick(fixed position) on scroll
     });
-		
+	$('.handle').trigger('click');	
 	//Initialize VIE
 	var myVIE = window.myVIE = new VIE();
     myVIE.loadSchemaOrg();
@@ -52,12 +52,36 @@ $(window).load(function () {
                         }
                     }],
 					end_query: function(){
-							$('.tag').click(function(ev){
-							var uri = $(this).attr('title');
-							$('#image_container')
-							.vieImageSearch({
-								entity: uri
-							});
+					debugger;
+							$(this).find('.tag').each(function(ev){
+							
+								var uri = $(this).attr('title');
+								var img_container = $(this).find('.tag_images');
+	
+								// set-up of the Image-widget
+								img_container
+									.vieImageSearch({
+										vie    : myVIE,
+										bin_size: 8,
+										services : {
+											flickr : {
+												api_key : "ffd6f2fc41249feeddd8e62a531dc83e",
+												use: true
+											},
+											gimage : {
+												use: true
+											}
+										},
+										render: render
+										
+								});
+								//start search
+								img_container
+									.vieImageSearch({
+										entity: uri,
+									});
+																				
+		
 					});}
                 });
 
@@ -74,43 +98,98 @@ $(window).load(function () {
                     filter: ["Place"],
                     label: ["Thing.name"],
 					end_query: function(){
-							$('.tag').click(function(ev){
-							var uri = $(this).attr('title');
-							$('#image_container')
-							.vieImageSearch({
-								entity: uri
-							});
+								$(this).find('.tag').each(function(ev){
+								var uri = $(this).attr('title');
+								var img_container = $(this).find('.tag_images');
+	
+								// set-up of the Image-widget
+								img_container
+									.vieImageSearch({
+										vie    : myVIE,
+										bin_size: 8,
+										services : {
+											flickr : {
+												api_key : "ffd6f2fc41249feeddd8e62a531dc83e",
+												use: true
+											},
+											gimage : {
+												use: true
+											}
+										}
+								});
+								//start search
+								img_container
+									.vieImageSearch({
+										entity: uri,
+									});
+																				
+		
 					});}
                 });
-	// Image search part
+	
             
-    // set-up of the Image-widget
-    $('#image_container')
-            .vieImageSearch({
-                vie    : myVIE,
-            	bin_size: 8,
-                services : {
-                    flickr : {
-                        api_key : "ffd6f2fc41249feeddd8e62a531dc83e",
-                    	use: true
-                    },
-                    gimage : {
-                        use: true
-                    }
-                }
-            });
-	var i = 10; 
-	$('#image_container img').each(function () {
-		i+= 10; 
-		$(this)
-		.css({
-			"z-index" : i, 
-			bottom: i + "px", 
-			position: "absolute", 
-			left: i + "px"})
-		.click(function () { 
-			$(this).css({"z-index" : 0});
-		})
-	});
+    render = function(data){
+			var self = this;
+            
+            var photos = self.options.photos;
+            var time = data.time;
+            
+            // clear the container element
+            $(self.element).empty();
+            //rendering
+            for (var p = 0; p < photos.length && p < this.options.bin_size; p++) {
+                var photo = photos[p];
+                var image = $('<a class="' + self.widgetBaseClass + '-image" target="_blank" href="' + photo.original + '"></a>')
+                    .append($('<img height="100px" width = "100px" src="' + photo.thumbnail + '" />'));
+                $(self.element).append(image);
+				if(p!=0){
+											$(image).css({
+													"z-index" : 0, 
+													position: "relative",
+													top: -p*100+"px",
+													opacity: 0.5,
+													left: p*30 + "px"})
+												.hover(function () { 
+													$(this).css({
+														"z-index":  10,
+														height: "120px",
+														width: "120px",
+														opacity: 1
+													})},
+												
+												function () { 
+													$(this).css({
+														"z-index":  0,
+														height: "100px",
+														width: "100px",
+														opacity: 0.5
+													})
+												});}
+				else{$(image).css({
+													"z-index" : 1, 
+													position: "relative",
+													top: -p*100+"px",
+													opacity: 1,
+													left: p*30 + "px"})
+												.hover(function () { 
+													$(this).css({
+														"z-index":  10,
+														height: "120px",
+														width: "120px",
+														opacity: 1
+													})},
+												
+												function () { 
+													$(this).css({
+														"z-index":  0,
+														height: "100px",
+														width: "100px",
+														opacity: 0.5
+													})
+												})
+													}
+			}						
+            
+            return this;}
 });
 
