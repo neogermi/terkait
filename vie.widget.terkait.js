583a78aa1b05556dac09ca600831afa04c3b4756
2529742 <hello.andrey@gmail.com>
2011-11-13 21:52:47 +0100

    added anythingSlider

--- vie.widget.terkait.js
+++ vie.widget.terkait.js
@@ -52,7 +52,6 @@ $(window).load(function () {
                         }
                     }],
 					end_query: function(){
-					debugger;
 							$(this).find('.tag').each(function(ev){
 							
 								var uri = $(this).attr('title');
@@ -137,59 +136,29 @@ $(window).load(function () {
             // clear the container element
             $(self.element).empty();
             //rendering
-            for (var p = 0; p < photos.length && p < this.options.bin_size; p++) {
-                var photo = photos[p];
-                var image = $('<a class="' + self.widgetBaseClass + '-image" target="_blank" href="' + photo.original + '"></a>')
-                    .append($('<img height="100px" width = "100px" src="' + photo.thumbnail + '" />'));
-                $(self.element).append(image);
-				if(p!=0){
-											$(image).css({
-													"z-index" : 0, 
-													position: "relative",
-													top: -p*100+"px",
-													opacity: 0.5,
-													left: p*30 + "px"})
-												.hover(function () { 
-													$(this).css({
-														"z-index":  10,
-														height: "120px",
-														width: "120px",
-														opacity: 1
-													})},
-												
-												function () { 
-													$(this).css({
-														"z-index":  0,
-														height: "100px",
-														width: "100px",
-														opacity: 0.5
-													})
-												});}
-				else{$(image).css({
-													"z-index" : 1, 
-													position: "relative",
-													top: -p*100+"px",
-													opacity: 1,
-													left: p*30 + "px"})
-												.hover(function () { 
-													$(this).css({
-														"z-index":  10,
-														height: "120px",
-														width: "120px",
-														opacity: 1
-													})},
-												
-												function () { 
-													$(this).css({
-														"z-index":  0,
-														height: "100px",
-														width: "100px",
-														opacity: 0.5
-													})
-												})
-													}
-			}						
-            
-            return this;}
-});
 
+			var preload_div = $('<div class="coda-slider preload">');
+			var ul = $('<ul>');
+			for (var p = 0; p < photos.length && p < this.options.bin_size; p++) {
+				var photo = photos[p];
+				var li = $('<li class="slider_item">');
+				var a = $('<a class="' + self.widgetBaseClass + '-image" target="_blank" href="' + photo.original + '"></a>');
+				var img = $('<img src="' + photo.thumbnail + '" />');
+				var div = $('<div>');
+				div.css({"height":"200px", "width": "200px"});
+				a.append(img);
+				div.append(a);
+				li.append(a);
+				ul.append(li);
+				
+			}
+			ul.appendTo($(self.element));
+            ul.anythingSlider({theme: 'minimalist-round'});
+			$('.tag_images img').each(function(){
+				debugger;
+				var img_pad = (150-$(this).height())/2+"px "+(200-$(this).width())/2+"px";
+				$(this).css({"max-height":"150px", "max-width": "200px", padding: img_pad,"background-color":"black"});
+			});	
+
+			return this;}
+});
